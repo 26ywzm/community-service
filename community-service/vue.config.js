2848e20c6d79d22a4e704edaadd81889d75fbd69
26ywzm <1972208498@qq.com>
2024-11-03 21:50:57 +0800

    修改配置

--- community-service/vue.config.js
+++ community-service/vue.config.js
@@ -1,4 +1,11 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  devServer: {
+    allowedHosts: [
+      'sq.26ywzm.icu',
+      'localhost',
+      '127.0.0.1',
+    ],
+  },
 })
