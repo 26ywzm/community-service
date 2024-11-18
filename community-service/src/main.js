cf1d93b4948553b8349edd785a9b8716fafa800a
26ywzm <1972208498@qq.com>
2024-11-18 22:41:50 +0800

    加入VantUI

--- community-service/src/main.js
+++ community-service/src/main.js
@@ -1,7 +1,10 @@
 import { createApp } from 'vue';  // 使用 Vue 3 的新导入方式
 import App from './App.vue';
 import router from './router'; // 确保导入 router
+import Vant from 'vant';
+import 'vant/lib/index.css';
 
 const app = createApp(App);
-app.use(router);  // 注册 router
+app.use(router);
+app.use(Vant);
 app.mount('#app');
